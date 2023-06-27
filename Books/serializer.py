a436b3c530b9c91458ff998e82df1ef34ff8ef8f
Shysk <shyshkov745@gmail.com>
2023-06-27 13:09:37 +0300

    Added user registration form, swagger and test api's using REST

---
+++ Books/serializer.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.hashers import make_password
+from django.db import models
+from rest_framework import serializers
+
+from .models import SimpleUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=255, required=True)
+    first_name = serializers.CharField(max_length=255, required=True)
+    created = serializers.DateTimeField()
+    email = serializers.CharField(max_length=255, required=True)
+    last_name = serializers.CharField(max_length=255, required=True)
+
+    class Meta:
+        model = SimpleUser
+        fields = ('id', 'created', 'first_name', 'last_name','email','password')
+
+    def create(self, validated_data):
+        user = SimpleUser.objects.create(
+            first_name=validated_data['first_name'],
+
+            email=validated_data['email'],
+
+            created=validated_data['created'],
+
+            last_name=validated_data['last_name'],
+
+            password=make_password(validated_data['password']),
+
+        )
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.email = validated_data.get("email", instance.email)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.created = validated_data.get("created", instance.created)
+        instance.password = validated_data.get("password", instance.password)
+        instance.save()
+        return instance
